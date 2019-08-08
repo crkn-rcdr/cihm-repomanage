891e8f4508edf6f8cc3edea2304a401bf876fc97
Russell McOrmond <Russell McOrmond rmcormond@crkn.ca>
2019-08-08 13:29:00 -0400

    Second set of Cron entries for managing swift -- only used on a single computer.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,6 +19,8 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/t
 # This seems to be owned by wrong user from time to time.
 mkdir -p /var/lock/tdr/
 chown tdr.tdr /var/lock/tdr/
+mkdir -p /var/log/tdr/
+chown tdr.tdr /var/log/tdr/
 
 cronandmail ()
 {
@@ -51,6 +53,14 @@ if [ "$1" = 'repomanage' ]; then
 # Replication check every 10 minutes (find work and put in queue, then run rsync to add to repository)
 0-59/10 * * * * tdr /bin/bash -c "tdr-replicationwork ; tdr-replicate"
 RMCRON
+        cronandmail
+elif [ "$1" = 'swiftmanage' ]; then
+	        cat <<-SRMCRON >>/etc/cron.d/repomanage
+# Repository Validation each evening
+#47 16 * * * tdr /bin/bash -c "date ; tdr verify --timelimit=43200 --maxprocs=8 ; date ; tdr walk ; date"
+# Replication check every 10 minutes (find work and put in queue, then run rsync to add to repository)
+5-59/10 * * * * tdr /bin/bash -c "tdr-swiftreplicationwork ; tdr-swiftreplicate"
+SRMCRON
         cronandmail
 else
     # Otherwise run what was asked as the 'tdr' user
