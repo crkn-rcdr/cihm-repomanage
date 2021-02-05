75bdc39e822ae5242c90256fe77f2666d4c6d369
Russell McOrmond <rmcormond@crkn.ca>
2021-02-05 18:33:07 -0500

    CouchDB 3 uses sharding, so the reduce needed to handle rereduce. https://github.com/crkn-rcdr/CIHM-TDR/issues/11

--- couchdb/tdrepo/design/tdr/views/newestaip.js
+++ couchdb/tdrepo/design/tdr/views/newestaip.js
@@ -7,7 +7,7 @@ module.exports = {
         emit(doc._id, [doc["manifest date"], "item"]);
     }
   },
-  reduce: function(keys, values) {
+  reduce: function(keys, values, rereduce) {
     var maxdate;
     for (i = 0; i < values.length; i++) {
       if (!values[i] || values[i][0] === undefined) continue;
@@ -19,7 +19,15 @@ module.exports = {
       var repolist = [];
       for (i = 0; i < values.length; i++) {
         if (!values[i] || values[i][0] === undefined) continue;
-        if (values[i][0] === maxdate) repolist.push(values[i][1]);
+        if (values[i][0] === maxdate) {
+          var myvalue=values[i][1];
+          // If this is a rereduce, then the value will already be an array
+          if (Array.isArray(myvalue)) {
+            repolist=repolist.concat(myvalue);
+          } else {
+            repolist.push(myvalue);
+          };
+        }
       }
       if (repolist) return [maxdate, repolist];
     }
